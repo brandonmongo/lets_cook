c9d03e513818fcb0d7896bb03b8361f0b038c112
brandonmongo <brandonmongo@hotmail.com>
2020-09-30 14:49:57 +0000

    setting up flask

---
+++ eny.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SERCET_KEY", "8lBRr{7NiTe8)7&5WNfXw6`jGioVn$")
+os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGODB_NAME", "cookbook")
